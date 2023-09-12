d52de946f9843237ebef198f08247e5b808d8803
Chris Collier <possiblellama@gmail.com>
2023-09-12 19:14:10 +0100

    feat: Prompt for messages and scope of JIRA messages

---
+++ cmd/inputs.go
@@ -0,0 +1,80 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/manifoldco/promptui"
+)
+
+var (
+	// Based on https://www.conventionalcommits.org/
+	// TODO allow config to choose between these lists
+	conventionTypes = []string{
+		"fix",
+		"feat",
+		"BREAKING CHANGE",
+	}
+	angularConventionTypes = []string{
+		"fix",
+		"feat",
+		"build",
+		"chore",
+		"ci",
+		"docs",
+		"style",
+		"refactor",
+		"perf",
+		"test",
+		"BREAKING CHANGE",
+	}
+)
+
+func promptType() string {
+	prefixPrompt := promptui.Select{
+		Label: "Select type of change",
+		Items: conventionTypes,
+	}
+	_, prefixAsStr, prefixErr := prefixPrompt.Run()
+	if prefixErr != nil {
+		fmt.Println("failed to select item from list", prefixErr)
+		os.Exit(1)
+	}
+	return strings.TrimSpace(prefixAsStr)
+}
+
+func promptScope() string {
+	scopePrompt := promptui.Prompt{
+		Label: "(Optional) Scope of change (JIRA or ClickUp ticket)",
+	}
+
+	scope, scopeErr := scopePrompt.Run()
+	if scopeErr != nil {
+		fmt.Println("failed to get scope", scopeErr)
+		os.Exit(1)
+	}
+
+	return strings.TrimSpace(scope)
+}
+
+func promptMessage() string {
+	messagePrompt := promptui.Prompt{
+		Label: "Commit message",
+		Validate: func(s string) error {
+			if len(s) < 4 {
+				return errors.New("message needs to be at least 4 characters")
+			}
+			return nil
+		},
+	}
+
+	message, messageErr := messagePrompt.Run()
+	if messageErr != nil {
+		fmt.Println("failed to get message", messageErr)
+		os.Exit(1)
+	}
+
+	return strings.TrimSpace(message)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -4,53 +4,27 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 
-	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
-)
-
-var (
-	// Based on https://www.conventionalcommits.org/
-	// TODO allow config to choose between these lists
-	conventionPrefix = []string{
-		"fix",
-		"feat",
-		"BREAKING CHANGE",
-	}
-	angularConventionPrefix = []string{
-		"fix",
-		"feat",
-		"build",
-		"chore",
-		"ci",
-		"docs",
-		"style",
-		"refactor",
-		"perf",
-		"test",
-		"BREAKING CHANGE",
-	}
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "commit-check",
 	Short: "Verify your commits have a common format",
 	Run: func(cmd *cobra.Command, args []string) {
-		prefixPrompt := promptui.Select{
-			Label: "Select type of change",
-			Items: conventionPrefix,
-		}
-		_, prefixAsStr, prefixErr := prefixPrompt.Run()
-		if prefixErr != nil {
-			fmt.Println("failed to select item from list", prefixErr)
-			os.Exit(1)
-		}
+		prefix := promptType()
+		scope := promptScope()
+		message := promptMessage()
 
 		// TODO get prefix from JIRA/Clickup/other
 		// TODO get input of messages
 
-		commitArgs := []string{"commit", "-m", prefixAsStr + ": " + strings.Join(args, " ")}
+		if scope != "" {
+			prefix = prefix + "(" + scope + "): "
+		} else {
+			prefix = prefix + ": "
+		}
+		commitArgs := []string{"commit", "-m", prefix + message}
 		runOsCmd := exec.Command("git", commitArgs...)
 
 		osCmdOutput, runErr := runOsCmd.CombinedOutput()
