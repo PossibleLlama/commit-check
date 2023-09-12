1f5eb7e1534fe32923fead173cd13932803320fb
Chris Collier <possiblellama@gmail.com>
2023-09-12 13:11:42 +0100

    feat: Add selector for prefix

--- cmd/root.go
+++ cmd/root.go
@@ -5,19 +5,56 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
+)
+
+var (
+	// Based on https://www.conventionalcommits.org/
+	// TODO allow config to choose between these lists
+	conventionPrefix = []string{
+		"fix",
+		"feat",
+		"BREAKING CHANGE",
+	}
+	angularConventionPrefix = []string{
+		"fix",
+		"feat",
+		"build",
+		"chore",
+		"ci",
+		"docs",
+		"style",
+		"refactor",
+		"perf",
+		"test",
+		"BREAKING CHANGE",
+	}
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "commit-check",
 	Short: "Verify your commits have a common format",
 	Run: func(cmd *cobra.Command, args []string) {
-		commitArgs := append([]string{"commit", "-m"}, args...)
+		prefixPrompt := promptui.Select{
+			Label: "Select type of change",
+			Items: conventionPrefix,
+		}
+		_, prefixAsStr, prefixErr := prefixPrompt.Run()
+		if prefixErr != nil {
+			fmt.Println("failed to select item from list", prefixErr)
+			os.Exit(1)
+		}
+
+		// TODO get input of message
+
+		commitArgs := append([]string{"commit", "-m", prefixAsStr, ": "}, args...)
 		runOsCmd := exec.Command("git", commitArgs...)
 
 		osCmdOutput, runErr := runOsCmd.CombinedOutput()
 		if runErr != nil {
 			fmt.Println("failed to run with error:", runErr.Error())
+			os.Exit(1)
 		}
 		fmt.Println(string(osCmdOutput))
 	},
