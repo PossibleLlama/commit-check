8c386319e2051bd4fe0b71a0cf9bff70b4ea84b0
Chris Collier <possiblellama@gmail.com>
2023-11-23 08:14:23 +0000

    feat: Add tests in CI

--- model/commits_test.go
+++ model/commits_test.go
@@ -52,3 +52,40 @@ func TestCommitString(t *testing.T) {
 		})
 	}
 }
+
+func TestCommitIsValid(t *testing.T) {
+	var tests = []struct {
+		name     string
+		commit   Commit
+		expected bool
+	}{
+		{
+			name: "valid commit",
+			commit: Commit{
+				Type:             CommitTypeFeat,
+				Scope:            "",
+				Description:      "add a new feature",
+				IsBreakingChange: false,
+			},
+			expected: true,
+		},
+		{
+			name: "invalid commit",
+			commit: Commit{
+				Type:             CommitTypeFeat,
+				Scope:            "",
+				Description:      "",
+				IsBreakingChange: false,
+			},
+			expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := test.commit.IsValid()
+
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
