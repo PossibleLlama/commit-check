13090ad754e78040ca013afb1e820978776a49e2
Chris Collier <29115613+PossibleLlama@users.noreply.github.com>
2025-04-15 22:26:02 +0000

    feat(86983pcud): Pull in scopes from plugins

--- tui/listFormat.go
+++ tui/listFormat.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/PossibleLlama/commit-check/model"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -48,4 +49,33 @@ func (d simpleListItemFormatter) Render(w io.Writer, m list.Model, index int, li
 	renderer := lipgloss.NewStyle().Align(lipgloss.Left, lipgloss.Center)
 
 	fmt.Fprint(w, renderer.Render(fmt.Sprintf("- %s", i)))
+}
+
+type scopeSelectableListItemFormatter struct{}
+
+func (d scopeSelectableListItemFormatter) Height() int                             { return 1 }
+func (d scopeSelectableListItemFormatter) Spacing() int                            { return 0 }
+func (d scopeSelectableListItemFormatter) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d scopeSelectableListItemFormatter) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(model.ScopeItem)
+	if !ok {
+		return
+	}
+
+	renderer := lipgloss.NewStyle().Align(lipgloss.Left, lipgloss.Center)
+	if m.Index() == index {
+		renderer = renderer.Bold(true)
+		if i.ID == "Other" || i.ID == "None" {
+			fmt.Fprint(w, renderer.Render(fmt.Sprintf("%s %s", cursor, i.Body)))
+		} else {
+			fmt.Fprint(w, renderer.Render(fmt.Sprintf("%s %s - %s", cursor, i.ID, i.Body)))
+		}
+		return
+	}
+
+	if i.ID == "Other" || i.ID == "None" {
+		fmt.Fprint(w, renderer.Render(fmt.Sprintf("- %s", i.Body)))
+	} else {
+		fmt.Fprint(w, renderer.Render(fmt.Sprintf("- %s - %s", i.ID, i.Body)))
+	}
 }
